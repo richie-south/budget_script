c3c304690b48c09bcfea953cd6f11584afb155b8
richie-south <richard.soderman@gmail.com>
2026-01-06 20:07:23 +0100

    feat: use budget script

---
+++ lang/src/index.ts
@@ -0,0 +1,10 @@
+import {enviroment} from './environment'
+import {evaluate} from './evaluator'
+import {parser} from './parser'
+
+export function budgetScript(src: string) {
+  const env = enviroment()
+  const ast = parser(src)
+
+  return evaluate(ast, env)
+}
