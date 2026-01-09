3a9dc103f067e79b90dc54db1b9b4e6a88ace5e5
richie-south <richard.soderman@gmail.com>
2026-01-09 21:29:38 +0100

    feat: evaluator error

--- lang/src/errors.ts
+++ lang/src/errors.ts
@@ -18,3 +18,24 @@ export class ParseError extends Error {
     Object.setPrototypeOf(this, ParseError.prototype)
   }
 }
+
+export class EvaluatorError extends Error {
+  public code: number
+  public line: number
+  public position: number
+
+  constructor(
+    message: string,
+    line: number,
+    position: number,
+    code: number = 400,
+  ) {
+    super(`Line ${line} position ${position}: ${message}`)
+    this.name = 'EvaluatorError'
+
+    this.code = code
+    this.line = line
+    this.position = position
+    Object.setPrototypeOf(this, ParseError.prototype)
+  }
+}
