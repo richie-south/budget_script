19b304544391673607f6d64edf34af3e0642f710
richie-south <richard.soderman@gmail.com>
2026-01-13 21:32:48 +0100

    chore: filter out print

--- lang/src/index.ts
+++ lang/src/index.ts
@@ -1,12 +1,19 @@
-import {enviroment} from './environment'
-import {evaluate} from './evaluator'
-import {parser} from './parser'
+import {enviroment} from './evaluator/enviroment/environment'
+import {evaluate, Print} from './evaluator/evaluator'
+import {parser} from './parser/parser'
 export * from './errors'
-export {parser} from './parser'
+export {parser} from './parser/parser'
 
-export function budgetScript(src: string) {
-  const env = enviroment()
+export function budgetScript(src: string): Print[] {
+  const date = new Date()
+  const env = enviroment(date)
   const ast = parser(src)
 
-  return evaluate(ast, env)
+  return evaluate(ast, env).filter((v) => {
+    if (Array.isArray(v)) {
+      return false
+    }
+
+    return v.type === 'print'
+  }) as Print[]
 }
